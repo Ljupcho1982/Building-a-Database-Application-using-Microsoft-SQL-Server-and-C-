cfc5936b183bba51c9ebde186ef4528e5c887251
Ljupcho Shemov <ljupco.semov@gmail.com>
2022-09-13 23:27:20 +0200

    Building a dataBase application with SQL and C#

---
+++ Building a Database Application/Building a Database Application/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+string connectionString = @"Data Source=LJUPCHO; Initial Catalog =ExampleDatabase; User ID=Ljupcho/Shemov;Password=;Trusted_Connection=Yes;";
+
+SqlConnection connection = new SqlConnection(connectionString);
+connection.Open();
+Console.WriteLine("Conected to the Server!");
+SqlCommand command = new SqlCommand();
+command.Connection = connection;
+command.CommandType = CommandType.Text;
+command.CommandText = "SELECT * FROM dbo.People";
+SqlDataReader reader = command.ExecuteReader();
+if (reader.HasRows)
+{
+
+    while (reader.Read())
+    {
+        Console.WriteLine(reader.GetString(1)+ "-" + reader.GetString(2));
+
+    }
+}
+
+connection.Close();
---
+++ SQLQuery1.sql
@@ -0,0 +1,6 @@
+CREATE TABLE People (id INT IDENTITY (1,1) PRIMARY KEY,
+name VARCHAR (100),
+age VARCHAR (3));
+
+INSERT INTO People (name, age) VALUES ('Rob',25),('John', 30),('Sarah',23),('Tim',40),('Bill',32);
+
